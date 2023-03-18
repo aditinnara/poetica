2bceed6b24ce1705f7d479e7073c18068854b743
aditinnara <aditin@andrew.cmu.edu>
2023-03-18 16:51:25 -0400

    created and updated register page added email, confirm password buttons

--- poetica/views.py
+++ poetica/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return render(request, "login_page.html")
+    return render(request, "register_page.html")
