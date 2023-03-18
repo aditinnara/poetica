a017deb4605fe0bcc19d09f177a2970fb199287f
aditinnara <aditin@andrew.cmu.edu>
2023-03-18 16:34:37 -0400

    created the login page, added background added random poem section, login section

--- poetica/views.py
+++ poetica/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return render(request, "profile_page.html")
+    return render(request, "login_page.html")
