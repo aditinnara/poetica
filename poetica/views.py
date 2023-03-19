88e071945672a8d3becf3a6f463ba35fecd3e865
aditinnara <aditin@andrew.cmu.edu>
2023-03-18 22:19:21 -0400

    added 'top poems' to navbar updated spacing on the mainpage

--- poetica/views.py
+++ poetica/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def profile(request):
-    return render(request, "discover_poem_page.html")
+    return render(request, "mainpage.html")
