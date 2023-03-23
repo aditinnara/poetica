af575f48746e9570e207d8f3002f48663dcc66db
Jaspreet Singh <jasprees@andrew.cmu.edu>
2023-03-22 21:33:27 -0400

    Updated models.py to include user profile and poem info

--- poetica/models.py
+++ poetica/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PoemInfo(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    bio = models.CharField(max_length=200)
+    profile_picture = models.FileField(blank=True)
+    content_type = models.CharField(max_length=50)
+    starred = models.ManyToManyField(PoemInfo)
